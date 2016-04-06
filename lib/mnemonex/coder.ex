c9c0b59789e62ff32df7f1b890f0871d406bf05d
Matt Miller <mwm@fastmail.com>
2016-04-06 22:44:27 +0200

    Improve words_to_bin.
    More functional.  More readable.  Still not right.

--- lib/mnemonex/coder.ex
+++ lib/mnemonex/coder.ex
@@ -18,32 +18,31 @@ defmodule Mnemonex.Coder do
   end
 
   def handle_call({:encode, string}, _from, state), do: {:reply, string |> gather_words(state,[]) |> format_mnx(state), state}
-  def handle_call({:decode, string}, _from, state), do: {:reply, string |> gather_numbers(state) |> words_to_bin(state, <<>>), state}
+  def handle_call({:decode, string}, _from, state), do: {:reply, string |> gather_numbers(state) |> words_to_bin(state.base_words, <<>>), state}
 
   defp gather_numbers(string,state), do: Regex.scan(~r/[a-z]+/, string |> String.downcase)
                                           |> List.flatten
                                           |> Enum.map( fn w -> Map.fetch!(state.word_indices, w) end)
 
+  defp x_to_bin(x,c), do: 0..(c-1) |> Enum.reduce(<<>>,fn s,a -> bsr(x,s*8)
+                                                          |> :binary.encode_unsigned(:little)
+                                                          |> binary_part(0,1) |> (&(a<>&1)).() end)
+  defp multi_add([a], _base, acc), do: acc + a
+  defp multi_add([x|rest], base, acc), do: multi_add(rest, base,x |> (&(acc + &1)).() |> (&(base * &1)).())
   defp words_to_bin([],_state, acc), do: acc
-  defp words_to_bin([a,b,c|rest],state, acc) do
-      base = state.base_words
-      extra = c >= base
-      x = if extra, do: c - base, else: c
-      x = x * base
-      x = x + b
-      x = x * base
-      x = x + a
-      res = if not extra, do: <<x, (x>>>8), (x>>>16), (x>>>24)>>, else: <<x, (x>>>8), (x>>>16)>>
-      words_to_bin(rest, state, acc<>res)
+  defp words_to_bin([a,b,c|rest],base, acc) when c >= base do
+      res =  [c - base, b, a] |> multi_add(base,0) |> x_to_bin(3)
+      words_to_bin(rest, base, acc<>res)
   end
-  defp words_to_bin([a,b],state, acc) do
-     base = state.base_words
-     x = b
-     x = x * base
-     x = x + a
-     words_to_bin([], state, acc<><<x, (x >>> 8)>>)
+  defp words_to_bin([a,b,c|rest],base, acc) when c < base do
+      res =  [c, b, a] |> multi_add(base,0) |> x_to_bin(4)
+      words_to_bin(rest, base, acc<>res)
   end
-  defp words_to_bin([a],state, acc), do: words_to_bin([], state, acc<><<a>>)
+  defp words_to_bin([a,b],base, acc) do
+     res = [b,a] |> multi_add(base,0) |> x_to_bin(2)
+     words_to_bin([], base, acc<>res)
+  end
+  defp words_to_bin([a],base, acc), do: words_to_bin([], base, acc<><<a>>)
 
   defp gather_words(<<>>,_state,acc), do: acc |> Enum.reverse |> List.flatten
   defp gather_words(<<a,b,c,d,rest::binary>>,state,acc) do
